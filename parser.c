49d1e6699d23abe88fc8bef0b2f29f143cf5842b
LipeAKugel <felipearaujodff@gmail.com>
2024-07-03 11:12:24 -0300

    declaring parser syntax functions

--- parser.c
+++ parser.c
@@ -10,6 +10,7 @@ void nextToken() {
     currentTok = currentTok->next;
 }
 
+// Checks if the current token matches the expected token type
 void matchToken(TokenType expected) {
   if (currentTok->tok->type == expected) {
     nextToken();
@@ -19,6 +20,7 @@ void matchToken(TokenType expected) {
   }
 }
 
+// Checks if the current token lexeme matches the expected lexeme
 void matchLexeme(char *expected) {
   if (strcmp(currentTok->tok->lexeme, expected) == 0) {
     nextToken();
@@ -28,12 +30,31 @@ void matchLexeme(char *expected) {
   }
 }
 
+// Parser functions
+void program();
+void block();
+void declarationList();
+void identifierList();
+void type();
+void statementList();
+void statement();
+void ifStatement();
+void whileStatement();
+void writeStatement();
+void writeParameters();
+void readStatement();
+void beginEndBlock();
+void expression();
+void term();
+void factor();
+
 void program() {
   matchLexeme("program");
   matchToken(IDENTIFIER);
   matchLexeme(";");
 }
 
+// Main parser function
 void parser(Node *tokenList) {
   currentTok = tokenList->next;
   program();
@@ -45,3 +66,38 @@ void parser(Node *tokenList) {
     exit(1);
   }
 }
+
+// Simplified Pascal Syntax
+// program         -> 'program' identifier ';' block '.'
+
+// block           -> declaration-list statement-list
+
+// declaration-list -> ('var' identifier-list ':' type ';')*
+
+// identifier-list -> identifier (',' identifier)*
+
+// type            -> 'integer' | 'real' | 'boolean'
+
+// statement-list  -> statement (';' statement)*
+
+// statement       -> assignment | if-statement | while-statement | write-statement | read-statement | begin-end-block
+
+// assignment      -> identifier ':=' expression
+
+// if-statement    -> 'if' expression 'then' statement ('else' statement)?
+
+// while-statement -> 'while' expression 'do' statement
+
+// write-statement -> 'write' '(' write-parameters ')'
+
+// write-parameters -> expression (',' expression)*
+
+// read-statement  -> 'read' '(' identifier-list ')'
+
+// begin-end-block -> 'begin' statement-list 'end'
+
+// expression      -> term (('+' | '-') term)*
+
+// term            -> factor (('' | '/') factor)
+
+// factor          -> identifier | number | '(' expression ')'
