f2ea6ef71f971e87dd1cf4a4cd37ab56adb1ca46
LipeAKugel <felipearaujodff@gmail.com>
2024-07-05 02:00:54 -0300

    refactoing parse

--- parser.c
+++ parser.c
@@ -1,40 +1,42 @@
 #include "header/parser.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 Node *currentTok;
 
-void nextToken()
-{
-  if (currentTok != NULL)
-    currentTok = currentTok->next;
+// Moves to the next token in the list
+void nextToken() {
+  if (currentTok != NULL) currentTok = currentTok->next;
 }
 
-// Checks if the current token matches the expected token type
-void matchToken(TokenType expected)
-{
-  if (currentTok->tok->type == expected)
-  {
+// Checks if current token type is the expected type
+int checkToken(TokenType expected) {
+  return currentTok->tok->type == expected;
+}
+
+// Checks if current lexeme is the expected lexeme
+int checkLexeme(char *expected) {
+  return (strcmp(currentTok->tok->lexeme, expected) == 0);
+}
+
+// Moves to the next token if current token type matches expected
+void matchToken(TokenType expected) {
+  if (checkToken(expected)) {
     nextToken();
-  }
-  else
-  {
-    printf("erro: unexpected token %d\n", currentTok->tok->type);
+  } else {
+    printf("error: unexpected token %d\n", currentTok->tok->type);
     exit(1);
   }
 }
 
-// Checks if the current token lexeme matches the expected lexeme
-void matchLexeme(char *expected)
-{
-  if (strcmp(currentTok->tok->lexeme, expected) == 0)
-  {
+// Moves to the next token if current lexeme matches expected
+void matchLexeme(char *expected) {
+  if (checkLexeme(expected)) {
     nextToken();
-  }
-  else
-  {
-    printf("erro: expected %s found %s\n", expected, currentTok->tok->lexeme);
+  } else {
+    printf("error: expected %s found %s\n", expected, currentTok->tok->lexeme);
     exit(1);
   }
 }
@@ -58,189 +60,161 @@ void term();
 void factor();
 
 void program() {
-    matchLexeme("program");
-    matchToken(IDENTIFIER);
-    matchLexeme(";");
-    block();
-    matchLexeme(".");
+  matchLexeme("program");
+  matchToken(IDENTIFIER);
+  matchLexeme(";");
+  block();
+  matchLexeme(".");
 }
 
 void block() {
-    declarationList();
-    statementList();
+  declarationList();
+  statementList();
 }
 
 void declarationList() {
-    while (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "var") == 0) {
-        matchLexeme("var");
-        identifierList();
-        matchLexeme(":");
-        type();
-        matchLexeme(";");
-    }
+  while (checkLexeme("var")) {
+    matchLexeme("var");
+    identifierList();
+    matchLexeme(":");
+    type();
+    matchLexeme(";");
+  }
 }
 
 void identifierList() {
+  matchToken(IDENTIFIER);
+  while (checkLexeme(",")) {
+    matchLexeme(",");
     matchToken(IDENTIFIER);
-    while (currentTok->tok->type == DELIMITER && strcmp(currentTok->tok->lexeme, ",") == 0) {
-        matchLexeme(",");
-        matchToken(IDENTIFIER);
-    }
+  }
 }
 
 void type() {
-    if (currentTok->tok->type == KEYWORD) {
-        if (strcmp(currentTok->tok->lexeme, "integer") == 0 || 
-            strcmp(currentTok->tok->lexeme, "real") == 0 || 
-            strcmp(currentTok->tok->lexeme, "boolean") == 0) {
-            nextToken();
-        } else {
-            printf("erro: tipo inválido %s\n", currentTok->tok->lexeme);
-            exit(1);
-        }
-    } else {
-        printf("erro: esperado tipo, encontrado %s\n", currentTok->tok->lexeme);
-        exit(1);
-    }
+  if (!checkToken(IDENTIFIER)) {
+    printf("erro: esperado tipo, encontrado %s\n", currentTok->tok->lexeme);
+    exit(1);
+  }
+
+  if (checkLexeme("integer") || checkLexeme("real") == 0 || checkLexeme("boolean")) {
+    matchToken(IDENTIFIER);
+  } else {
+    printf("erro: tipo inválido %s\n", currentTok->tok->lexeme);
+    exit(1);
+  }
 }
 
 void statementList() {
+  statement();
+  while (checkLexeme(";")) {
+    matchLexeme(";");
     statement();
-    while (currentTok->tok->type == DELIMITER && strcmp(currentTok->tok->lexeme, ";") == 0) {
-        matchLexeme(";");
-        statement();
-    }
+  }
 }
 
 void statement() {
-    if (currentTok->tok->type == IDENTIFIER) {
-        // assignment
-        matchToken(IDENTIFIER);
-        matchLexeme(":=");
-        expression();
-    } else if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "if") == 0) {
-        // if-statement
-        ifStatement();
-    } else if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "while") == 0) {
-        // while-statement
-        whileStatement();
-    } else if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "write") == 0) {
-        // write-statement
-        writeStatement();
-    } else if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "read") == 0) {
-        // read-statement
-        readStatement();
-    } else if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "begin") == 0) {
-        // begin-end-block
-        beginEndBlock();
-    } /*else if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "end") == 0) {
-        // begin-end-block
-        beginEndBlock();
-    }*/ else {
-        printf("erro: declaração inválida %s\n", currentTok->tok->lexeme);
-        exit(1);
-    }
+  if (checkToken(IDENTIFIER)) {
+    // assignment
+    matchToken(IDENTIFIER);
+    matchLexeme(":=");
+    expression();
+  } 
+  else if (checkLexeme("if")) ifStatement();
+  else if (checkLexeme("while")) whileStatement();
+  else if (checkLexeme("write")) writeStatement();
+  else if (checkLexeme("read")) readStatement();
+  else if (checkLexeme("begin")) beginEndBlock();
+  else {
+    printf("erro: declaração inválida %s\n", currentTok->tok->lexeme);
+    exit(1);
+  }
 }
 
 void ifStatement() {
-    matchLexeme("if");
-    expression();
-    matchLexeme("then");
+  matchLexeme("if");
+  expression();
+  matchLexeme("then");
+  statement();
+  if (checkLexeme("else")) {
+    matchLexeme("else");
     statement();
-    if (currentTok->tok->type == KEYWORD && strcmp(currentTok->tok->lexeme, "else") == 0) {
-        matchLexeme("else");
-        statement();
-    }
+  }
 }
 
 void whileStatement() {
-    matchLexeme("while");
-    expression();
-    matchLexeme("do");
-    statement();
+  matchLexeme("while");
+  expression();
+  matchLexeme("do");
+  statement();
 }
 
 void writeStatement() {
-    matchLexeme("write");
-    matchLexeme("(");
-    writeParameters();
-    matchLexeme(")");
+  matchLexeme("write");
+  matchLexeme("(");
+  writeParameters();
+  matchLexeme(")");
 }
 
 void writeParameters() {
+  expression();
+  while (checkLexeme("m")) {
+    matchLexeme(",");
     expression();
-    while (currentTok->tok->type == DELIMITER && strcmp(currentTok->tok->lexeme, ",") == 0) {
-        matchLexeme(",");
-        expression();
-    }
+  }
 }
 
 void readStatement() {
-    matchLexeme("read");
-    matchLexeme("(");
-    identifierList();
-    matchLexeme(")");
+  matchLexeme("read");
+  matchLexeme("(");
+  identifierList();
+  matchLexeme(")");
 }
 
 void beginEndBlock() {
-    matchLexeme("begin");
-    statementList();
-    matchLexeme("end");
+  matchLexeme("begin");
+  statementList();
+  matchLexeme("end");
 }
 
 void expression() {
+  term();
+  while (checkToken(OPERATOR) && (checkLexeme("+") || checkLexeme("-") == 0)) {
+    nextToken();
     term();
-    while (currentTok->tok->type == OPERATOR && 
-           (strcmp(currentTok->tok->lexeme, "+") == 0 || strcmp(currentTok->tok->lexeme, "-") == 0)) {
-        nextToken();
-        term();
-    }
+  }
 }
 
 void term() {
+  factor();
+  while (checkToken(OPERATOR) && (checkLexeme("*") || checkLexeme("/"))) {
+    nextToken();
     factor();
-    while (currentTok->tok->type == OPERATOR && 
-           (strcmp(currentTok->tok->lexeme, "*") == 0 || strcmp(currentTok->tok->lexeme, "/") == 0)) {
-        nextToken();
-        factor();
-    }
+  }
 }
 
-void factor()
-{
-  if (currentTok->tok->type == IDENTIFIER)
-  {
+void factor() {
+  if (checkToken(IDENTIFIER)) {
     matchToken(IDENTIFIER);
-  }
-  else if (currentTok->tok->type == NUMBER)
-  {
+  } else if (checkToken(NUMBER)) {
     matchToken(NUMBER);
-  }
-  else if (currentTok->tok->type == DELIMITER && strcmp(currentTok->tok->lexeme, "(") == 0)
-  {
+  } else if (checkLexeme("(")) {
     matchLexeme("(");
     expression();
     matchLexeme(")");
-  }
-  else
-  {
+  } else {
     printf("erro: fator inválido %s\n", currentTok->tok->lexeme);
     exit(1);
   }
 }
 
 // Main parser function
-void parser(Node *tokenList)
-{
+void parser(Node *tokenList) {
   currentTok = tokenList->next;
   program();
 
-  if (currentTok == NULL)
-  {
+  if (currentTok == NULL) {
     printf("Aceito\n");
-  }
-  else
-  {
+  } else {
     printf("Rejeito\n");
     exit(1);
   }
@@ -259,7 +233,8 @@ void parser(Node *tokenList)
 
 // statement-list  -> statement (';' statement)*
 
-// statement       -> assignment | if-statement | while-statement | write-statement | read-statement | begin-end-block
+// statement       -> assignment | if-statement | while-statement |
+// write-statement | read-statement | begin-end-block
 
 // assignment      -> identifier ':=' expression
 
